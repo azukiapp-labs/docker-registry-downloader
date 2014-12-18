db5df2c7be5a1fd3ba6845d68e8b5819bec93bd9
Julio Makdisse Saito <saitodisse@gmail.com>
2014-12-18 18:42:49 -0200

    Exporting all classes to index

---
+++ examples/sync-azktcl-example/start.js
@@ -0,0 +1,4 @@
+var DockerHub   = require('docker-registry-downloader').DockerHub;
+var Syncronizer = require('docker-registry-downloader').Syncronizer;
+var dockerHub = new DockerHub();
+var syncronizer = new Syncronizer();
--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ require('traceur');
 
 import DockerHub from './docker-hub';
 import DockerRegistry from './docker-registry';
+import DockerRemote from './docker-remote';
+import Syncronizer from './syncronizer';
 
 module.exports = {
 	DockerHub: DockerHub,
-	DockerRegistry: DockerRegistry
+	DockerRegistry: DockerRegistry,
+	DockerRemote: DockerRemote,
+	Syncronizer: Syncronizer,
 };
