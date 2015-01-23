4833380b87c34321925a7a89e6f23a3834e9acbb
Julio Makdisse Saito <saitodisse@gmail.com>
2015-01-23 00:24:29 -0200

    Build before publishing

--- script/publishPatch.sh
+++ script/publishPatch.sh
@@ -3,7 +3,7 @@
 while true; do
     read -p "Do you want to publish a patch? [y/n]: " yn
     case $yn in
-        [Yy]* ) npm version patch; git push; sudo npm publish .;break;;
+        [Yy]* ) gulp build; npm version patch; git push; sudo npm publish .;break;;
         [Nn]* ) exit;;
         * ) echo "Please answer yes or no.";;
     esac
